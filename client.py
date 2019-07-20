d371d105dab7be33a06113a7be2d72429af7ebf5
newnativeabq <42101276+newnativeabq@users.noreply.github.com>
2019-07-20 14:34:59 -0600

    initial commit

---
+++ client.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Client Op
+Created on Sat Jul 20 14:26:14 2019
+
+gets credectials from cred.py and password
+takes search terms and sends requests to mendeley
+exports returned objects to storage.
+
+@author: vince
+"""
+
